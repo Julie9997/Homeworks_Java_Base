8b0dfcf7fe562ca5f113a99aab6290d9c073a0ef
Julie9997 <ula.9997@mail.ru>
2023-02-13 23:46:41 +0300

    Task3

--- Homework1/Task3.java
+++ Homework1/Task3.java
@@ -1,5 +1,37 @@
 package Homework1;
 
+import java.util.Scanner;
+
+// Реализовать простой калькулятор (+ - / *)
+
 public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите первое чсло: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Выберите действие ( + | - | / | * ): ");
+        String operation = sc.next();
+        System.out.print("Введите второе чсло: ");
+        double num2 = sc.nextDouble();
+        sc.close();
+
+        switch(operation){
+            case "+":
+                System.out.println(num1 + " " + operation + " " + num2 + " = " + (num1 + num2));
+                break;
+            case "-":
+                System.out.println(num1 + " " + operation + " " + num2 + " = " + (num1 - num2));
+                break;
+            case "*":
+                System.out.println(num1 + " " + operation + " " + num2 + " = " + (num1 * num2));
+                break;
+            case "/":
+                System.out.println(num1 + " " + operation + " " + num2 + " = " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Незнакомая операция!");
+        }
+    }
     
 }
