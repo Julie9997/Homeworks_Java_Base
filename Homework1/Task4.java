70aa320ee253f6065e6465835f51930009e99481
Julie9997 <ula.9997@mail.ru>
2023-02-14 05:28:41 +0300

    Task4

--- Homework1/Task4.java
+++ Homework1/Task4.java
@@ -6,9 +6,52 @@ package Homework1;
 
 import java.util.Scanner;
 
+// в данном решении предполагется, что слагаемые в уравнении обязательно двузначные числа
 public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите уравнение вида q + w = e, q, w, e >= 0, заменив чась");
+        System.out.print("Введите уравнение, например 2? + ?5 = 69: ");
+        String[] expression = sc.nextLine().split(" ");
+        String num1 = expression[0];
+        String num2 = expression[2];
+        int result = Integer.parseInt(expression[4]);
+        sc.close();
+        
+        int count = 0;
+
+        int q = toNumber(num1);
+        int mul1 = 1;
+        if(q < 10)
+            mul1 = 10;
+        int w = toNumber(num2);
+        int mul2 = 1;
+        if(w < 10)
+            mul2 = 10;
+
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(j == 0 && mul2 == 10){
+                    j++;
+                    continue;
+                }
+                if(((q + i * mul1) + (w + j * mul2)) == result){
+                    System.out.println((q + i * mul1) + " + " + (w + j * mul2) + " = " + result);
+                    count++;
+                }
+            }
+        }
+
+        if(count == 0)
+            System.out.println("Решения нет");
+        
+    }
+
+    public static int toNumber(String num){
+        int n;
+        if(num.charAt(0) == '?')
+            n = Character.getNumericValue(num.charAt(1));
+        else
+            n = Character.getNumericValue(num.charAt(0)) * 10;
+        return n;
     }
 }
